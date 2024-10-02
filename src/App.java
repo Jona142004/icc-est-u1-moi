672458cd83343fcae843b40eabbd356a97daea5b
Jonnathan Parraga <jonnathanhola@gmail.com>
2024-10-02 11:57:26 -0500

    actualizado

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Insercion ordenar = new Insercion();
-        int[] arreglo = {9,8,7,6,5};
-        
-        
+        int[] arreglo = {-3,5,7,2,90,6};
+        int [] arregloordenado = ordenar.sortbyInsercion(arreglo);
+        ordenar.printArreglo(arregloordenado);
     } 
 }
--- src/Insercion.java
+++ src/Insercion.java
@@ -1,3 +1,20 @@
 public class Insercion {
-
+    public int[] sortbyInsercion(int[] arreglo){
+        int tam = arreglo.length;
+        for (int i = 1; i < tam; i++ ){
+            int aux = arreglo[i];
+            int j = i - 1;
+            while (j >= 0  && arreglo[j] > aux) {
+                arreglo[j + 1] = arreglo[j];
+                j = j - 1; 
+                arreglo[j + 1] = aux;
+            }       
+        }
+        return arreglo;
+    }
+    public void printArreglo(int[]arreglo){
+        for (int elemento : arreglo){ //for each el arreglo va instanciando cada elemento del arreglo se va almacenando
+            System.out.print(elemento + ",");
+        }
+    }
 }
