fbbd80b7a8049b6d7a227113c2c12859a44ef382
Jonnathan Parraga <jonnathanhola@gmail.com>
2024-10-02 11:26:06 -0500

    hola amigos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        Insercion ordenar = new Insercion();
+        int[] arreglo = {9,8,7,6,5};
+        
+        
+    } 
 }
