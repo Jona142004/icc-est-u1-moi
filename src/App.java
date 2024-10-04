8c9f5e37ce827c690c411a1884a872fe1f8660af
Jonnathan Parraga <jonnathanhola@gmail.com>
2024-10-04 11:21:05 -0500

    actual

--- src/App.java
+++ src/App.java
@@ -1,8 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Insercion ordenar = new Insercion();
+        boolean logs;
         int[] arreglo = {-3,5,7,2,90,6};
-        int [] arregloordenado = ordenar.sortbyInsercion(arreglo);
+        int [] arregloordenado = ordenar.sortbyInsercion(arreglo,true);
         ordenar.printArreglo(arregloordenado);
+        ordenar.sortbyInsercion(arreglo,true);
+        ordenar.printArreglo(arreglo);
+        System.out.print("Insercion");
+        ordenar.sortbyInsercion(arreglo,false);
+        ordenar.printArreglo(arreglo);
     } 
 }
--- src/Insercion.java
+++ src/Insercion.java
@@ -1,14 +1,35 @@
+import java.util.Arrays;
 public class Insercion {
-    public int[] sortbyInsercion(int[] arreglo){
+    public int[] sortbyInsercion(int[] arreglo, boolean logs){
         int tam = arreglo.length;
         for (int i = 1; i < tam; i++ ){
+            if(logs){
+                System.out.println("Pasada numero" + i);
+            }
+            
             int aux = arreglo[i];
             int j = i - 1;
+            if (logs) {
+                System.out.println("\ti=" + i + " j=" + j + " [i]=" + arreglo[i] + " [j]=" + arreglo[j]);
+            }
+            
             while (j >= 0  && arreglo[j] > aux) {
+                if (logs) {
+                    System.out.println("\t\tCompramos " + aux + " con " + arreglo[j]);  
+                }
+                
                 arreglo[j + 1] = arreglo[j];
                 j = j - 1; 
                 arreglo[j + 1] = aux;
-            }       
+                if(logs){
+                    System.out.println("\t\t--------" + Arrays.toString(arreglo));
+                }
+                
+            } 
+            if (logs) {
+                System.out.println("\t--------" + Arrays.toString(arreglo));
+            }
+                  
         }
         return arreglo;
     }
